180684fd51cb9dc74657d975ed92c30f7487466e
datsideofthemoon <datsideofthemoon@gmail.com>
2014-06-10 11:45:24 +0400

    Fixed undefined tag parameter in tagreader.py.

--- streamserver/tagreader.py
+++ streamserver/tagreader.py
@@ -25,136 +25,138 @@ class FileNotSupported(Exception):
 
 class Tags:
     tag_groups = {
-#id3
-        "TCON":"genre",
-        "TIT1":"grouping",
-        "TIT2":"title",
-        "TIT3":"version",
-        "TPE1":"artist",
-        "TPE2":"performer",
-        "TPE3":"conductor",
-        "TPE4":"arranger",
-        "TEXT":"lyricist",
-        "TCOM":"composer",
-        "TENC":"encodedby",
-        "TALB":"album",
-        "TRCK":"tracknumber",
-        "TPOS":"discnumber",
-        "TSRC":"isrc",
-        "TCOP":"copyright",
-        "TPUB":"organization",
-        "TSST":"discsubtitle",
-        "TOLY":"author",
-        "TMOO":"mood",
-        "TBPM":"bpm",
-        "TDRC":"date",
-        "TDOR":"originaldate",
-        "TOAL":"originalalbum",
-        "TOPE":"originalartist",
-        "WOAR":"website",
-        "TSOP":"artistsort",
-        "TSOA":"albumsort",
-        "TSOT":"titlesort",
-        "TSO2":"albumartistsort",
-        "TSOC":"composersort",
-        "TMED":"media",
-        "TCMP":"compilation",
+        #id3
+        "TCON": "genre",
+        "TIT1": "grouping",
+        "TIT2": "title",
+        "TIT3": "version",
+        "TPE1": "artist",
+        "TPE2": "performer",
+        "TPE3": "conductor",
+        "TPE4": "arranger",
+        "TEXT": "lyricist",
+        "TCOM": "composer",
+        "TENC": "encodedby",
+        "TALB": "album",
+        "TRCK": "tracknumber",
+        "TPOS": "discnumber",
+        "TSRC": "isrc",
+        "TCOP": "copyright",
+        "TPUB": "organization",
+        "TSST": "discsubtitle",
+        "TOLY": "author",
+        "TMOO": "mood",
+        "TBPM": "bpm",
+        "TDRC": "date",
+        "TDOR": "originaldate",
+        "TOAL": "originalalbum",
+        "TOPE": "originalartist",
+        "WOAR": "website",
+        "TSOP": "artistsort",
+        "TSOA": "albumsort",
+        "TSOT": "titlesort",
+        "TSO2": "albumartistsort",
+        "TSOC": "composersort",
+        "TMED": "media",
+        "TCMP": "compilation",
 
-#flac
-        "date":"date",
-        "tracknumber":"tracknumber",
-        "genre":"genre",
-        "artist":"artist",
-        "comment":"comment",
-        "album":"album",
-        "discid":"discid",
-        "title":"title",
+        #flac
+        "date": "date",
+        "tracknumber": "tracknumber",
+        "genre": "genre",
+        "artist": "artist",
+        "comment": "comment",
+        "album": "album",
+        "discid": "discid",
+        "title": "title",
 
-#apev2
-        "subtitle":"version",
-        "track":"tracknumber",
-        "disc":"discnumber",
-        "catalog":"labelid",
-        "year":"date",
-        "record location":"location",
-        "album artist":"albumartist",
-        "debut album":"originalalbum",
-        "record date":"recordingdate",
-        "original artist":"originalartist",
-        "mixartist":"remixer",
-#ape
-        "Genre":"genre",
+        #apev2
+        "subtitle": "version",
+        "track": "tracknumber",
+        "disc": "discnumber",
+        "catalog": "labelid",
+        "year": "date",
+        "record location": "location",
+        "album artist": "albumartist",
+        "debut album": "originalalbum",
+        "record date": "recordingdate",
+        "original artist": "originalartist",
+        "mixartist": "remixer",
+        #ape
+        "Genre": "genre",
         "Year": "date",
-        "Artist":"artist",
+        "Artist": "artist",
         "Album": "album",
         "Track": "tracknumber",
-        "Title":"title",
-#m4a
+        "Title": "title",
+        #m4a
         b'\xa9nam': "title",
         b'\xa9ART': "artist",
-        b'aART': "albumartist", #???
+        b'aART': "albumartist",  #???
         b'\xa9alb': "album",
         b'trkn': "tracknumber",
         b'disk': "discnumber",
-        b'\xa9gen':"genre",
+        b'\xa9gen': "genre",
         b'\xa9day': "date",
         b'cprt': "copyright",
-        b'xid ' :"discid"
-}
+        b'xid ': "discid"
+    }
 
-    def parse_mutagen_value(self,data):
-        if isinstance(data,str):
+    def parse_mutagen_value(self, data):
+        if isinstance(data, str):
             return data
-        elif isinstance(data,tuple):
-            var=list()
+        elif isinstance(data, tuple):
+            var = list()
             var.append(str(i) for i in data)
-            var=" ".join(var)
+            var = " ".join(var)
             return var
-        elif isinstance(data,list):
-            if isinstance(data[0],tuple):
-                var=list()
+        elif isinstance(data, list):
+            if isinstance(data[0], tuple):
+                var = list()
                 for i in data[0]: var.append(str(i))
                 return " ".join(var)
             else:
                 return " ".join(data)
-        elif isinstance(data,mutagenx.apev2.APETextValue):
+        elif isinstance(data, mutagenx.apev2.APETextValue):
             return data.value
         else:
-            if isinstance(data,mutagenx.id3.TDRC):
+            if isinstance(data, mutagenx.id3.TDRC):
+                return data.text[0].text
+            elif isinstance(data, mutagenx.id3.TDOR):
                 return data.text[0].text
             else:
                 return " ".join(data.text)
 
-    def length_from_sec(self,seconds):
+    def length_from_sec(self, seconds):
         m, s = divmod(seconds, 60)
         h, m = divmod(m, 60)
-        if h>0:
+        if h > 0:
             return "%d:%02d:%02d" % (h, m, s)
         else:
             return "%02d:%02d" % (m, s)
 
-    def get_length(self,mutagen_info,file,size):
-        if hasattr(mutagen_info,'length'):
-            length=self.length_from_sec(mutagen_info.length)
+    def get_length(self, mutagen_info, file, size):
+        if hasattr(mutagen_info, 'length'):
+            length = self.length_from_sec(mutagen_info.length)
         else:
-            bit=self.get_bitrate(mutagen_info,file,size)
-            if bit>0:
-                length=self.length_from_sec((size*8)/bit)
+            bit = self.get_bitrate(mutagen_info, file, size)
+            if bit > 0:
+                length = self.length_from_sec((size * 8) / bit)
             else:
-                length='<Unknown>'
+                length = '<Unknown>'
         return length
 
-    def get_bitrate(self, mutagen_info, file,size):
+    def get_bitrate(self, mutagen_info, file, size):
         if hasattr(mutagen_info, 'bitrate'):
-            bit = mutagen_info.bitrate/1000
+            bit = mutagen_info.bitrate / 1000
         else:
             if hasattr(mutagen_info, 'length'):
-                bit = (size*8)/(mutagen_info.length*1000)
+                bit = (size * 8) / (mutagen_info.length * 1000)
             else:
-                bit=0 #TODO:proper bitrate and length
+                bit = 0  #TODO:proper bitrate and length
         return bit
 
-    def __init__(self,file):
+    def __init__(self, file):
         self.filename = file
         self.mutagen_file = mutagenx.File(file)
         #self.format=type(self.mutagen_file)
@@ -169,20 +171,19 @@ class Tags:
         self.date = None
         self.comment = None
         #self.copyright=None
-        size = os.path.getsize(file)/1024/1024
-        self.size=str("%.1f"%size)+' mb'
+        size = os.path.getsize(file) / 1024 / 1024
+        self.size = str("%.1f" % size) + ' mb'
         #bitrate in 'xxx kbps'
-        bit=self.get_bitrate(self.mutagen_file.info,file,size)
-        if bit>0:
-            self.bitrate=str("%.f" % bit)+" kbps"
+        bit = self.get_bitrate(self.mutagen_file.info, file, size)
+        if bit > 0:
+            self.bitrate = str("%.f" % bit) + " kbps"
         else:
-            self.bitrate='<Unknown>'
+            self.bitrate = '<Unknown>'
 
         #length in 'h:m:s'
-        self.length=self.get_length(self.mutagen_file.info,file,size)
+        self.length = self.get_length(self.mutagen_file.info, file, size)
 
-
-        for key,value in self.mutagen_file.items():
+        for key, value in self.mutagen_file.items():
             if key in self.tag_groups.keys():
                 setattr(self, self.tag_groups[key], self.parse_mutagen_value(value))
 
